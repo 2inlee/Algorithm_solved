edcc16893beb6f78fb02bee6eeac0dab2a3e2bff
inho <95608811+AlphateamInho@users.noreply.github.com>
2022-09-20 18:04:43 +0900

    [Bronze II] Title: 수 정렬하기, Time: 312 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+numbers = []
+
+for _ in range(N) : 
+    numbers.append(int(input()))
+
+for i in range(len(numbers)) : 
+    for j in range(len(numbers)) : 
+        if numbers[i] < numbers[j] : 
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            
+for n in numbers : 
+    print(n)
