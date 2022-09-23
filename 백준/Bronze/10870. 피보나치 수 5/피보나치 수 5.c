74e4feba739ee5e9983723c10f7ae565123bed21
inho <95608811+AlphateamInho@users.noreply.github.com>
2022-09-23 16:13:15 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int fibo(int n) {
+    if (n >= 2)
+        return fibo(n - 1) + fibo(n - 2);
+    else if (n == 1) return 1;
+    else return 0;
+}
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fibo(n));
+    return 0;
+}
