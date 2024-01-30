35fbf5b44ad06a6470baad983297a6d27d1fa795
Inho Lee <95608811+2inlee@users.noreply.github.com>
2024-01-30 22:59:46 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT 
+    ugb.TITLE, 
+    ugb.BOARD_ID, 
+    ugr.REPLY_ID, 
+    ugr.WRITER_ID, 
+    ugr.CONTENTS, 
+    DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+FROM 
+    USED_GOODS_BOARD ugb
+JOIN 
+    USED_GOODS_REPLY ugr ON ugb.BOARD_ID = ugr.BOARD_ID
+WHERE 
+    DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
+ORDER BY 
+    ugr.CREATED_DATE ASC, 
+    ugb.TITLE ASC;
