5d49791be9f0669264a548f42956049f9a1a6b3d
2inlee <kbs@kakao.com>
2024-02-16 23:38:37 +0900

    마구간정하기

---
+++ 파이썬알고리즘/이분탐색/마구간정하기.py
@@ -0,0 +1,27 @@
+n, c = map(int, input().split())
+line = []
+for _ in range(n):
+  line.append(int(input()))
+line.sort()
+
+def Count(len):
+  cnt = 1
+  ep = line[0]
+  for i in range(1, n):
+    if line[i] - ep >= len:
+      cnt += 1
+      ep = line[i]
+  return cnt
+
+lt = 1
+rt = line[n-1]
+while lt <= rt:
+  mid = (lt + rt) // 2
+  if Count(mid) >= c:
+    res = mid
+    lt = mid + 1
+  else:
+    rt = mid - 1
+
+print(res)
+
