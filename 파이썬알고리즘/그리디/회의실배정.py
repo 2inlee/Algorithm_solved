633dddfda84061dbb336909f82f209fda75a4f7a
2inlee <kbs@kakao.com>
2024-02-16 23:55:14 +0900

    회의실 수정

--- 파이썬알고리즘/그리디/회의실배정.py
+++ 파이썬알고리즘/그리디/회의실배정.py
@@ -1,11 +1,9 @@
-# 그리디 문제는 대부분 정렬을 하고 풀어야 한다.
-
 n = int(input())
 meeting = []
 for i in range(n):
   s, e = map(int, input().split())
   meeting.append((s, e))
-  meeting.sort(key=lambda x: (x[1], x[0]))
+  meeting.sort(key=lambda x: (x[1], x[0])) #그리디 문제는 정렬이 중요한데, 어떤 것을 기준으로 정렬할 지 람다식으로 풀 수 있다.
   et = 0
   cnt = 0
 
