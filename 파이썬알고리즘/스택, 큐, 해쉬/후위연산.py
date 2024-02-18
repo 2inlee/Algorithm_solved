e2e0a4254d77cbaa31159259c12003d6aa07fc10
2inlee <kbs@kakao.com>
2024-02-18 13:52:23 +0900

    후위연산

---
+++ 파이썬알고리즘/스택, 큐, 해쉬/후위연산.py
@@ -0,0 +1,24 @@
+a = input()
+stack = []
+for x in a:
+  if x.isdecimal():
+    stack.append(int(x))
+  else:
+    if x == '+':
+      n1 = stack.pop()
+      n2 = stack.pop()
+      stack.append(n2+n1)
+    elif x == '-':
+      n1 = stack.pop()
+      n2 = stack.pop()
+      stack.append(n2-n1)
+    elif x == '*':
+      n1 = stack.pop()
+      n2 = stack.pop()
+      stack.append(n2*n1)
+    elif x == '/':
+      n1 = stack.pop()
+      n2 = stack.pop()
+      stack.append(n2/n1)
+
+print(stack[0])
