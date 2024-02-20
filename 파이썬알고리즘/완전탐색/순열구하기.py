c718958ca774bcbb84f5c82c27e9f479e248b948
2inlee <kbs@kakao.com>
2024-02-21 00:44:07 +0900

    edit 순열구하기 arg

--- 파이썬알고리즘/완전탐색/순열구하기.py
+++ 파이썬알고리즘/완전탐색/순열구하기.py
@@ -1,4 +1,4 @@
-def DFS(L, n, m):
+def DFS(L):
   global cnt
   if L == m:  # m개를 모두 선택했으면 출력
     print(' '.join(map(str, combination)))
@@ -8,7 +8,7 @@ def DFS(L, n, m):
     if not ch[i]:  # 아직 선택되지 않았으면 (0으로 표시)
       ch[i] = 1  # 선택 상태로 변경 (1로 표시) 
       combination[L] = i  # 현재 위치에 구슬 번호를 할당
-      DFS(L + 1, n, m)  # 다음 구슬 선택을 위해 재귀 호출
+      DFS(L+1)  # 다음 구슬 선택을 위해 재귀 호출
       ch[i] = 0  # 방문 상태를 되돌림 (0으로 표시)
 
 if __name__ == "__main__":
@@ -16,6 +16,6 @@ if __name__ == "__main__":
   cnt = 0  # 경우의 수를 세기 위한 변수
   ch = [0] * (n + 1)  # 각 구슬의 선택 여부를 체크하기 위한 리스트 (0으로 초기화)
   combination = [0] * m  # 현재까지 선택된 구슬의 조합을 저장하는 리스트
-  DFS(0, n, m)  # DFS 시작
+  DFS(0)  # DFS 시작
   print(cnt)  # 총 경우의 수 출력
  
