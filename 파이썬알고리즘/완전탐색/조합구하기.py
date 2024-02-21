4549f90f77ce8861f136a9bb404ff336b9747e84
2inlee <kbs@kakao.com>
2024-02-21 23:38:20 +0900

    조합구하기

--- 파이썬알고리즘/완전탐색/조합구하기.py
+++ 파이썬알고리즘/완전탐색/조합구하기.py
@@ -1,25 +1,19 @@
-# 4 2 -> 1 2 / 1 3 / 1 4 / 2 3 / 2 4 / 3 4
-# 맨 앞꺼 냅두고 그 다음거 냅두고 그 다다음것부터 쓰면 되는데, 이게 썼던거 또 쓰면 안되고, 뒤에 나오는 숫자가 앞에꺼보다 더 크면 안된 다는 점을 조건
-# ch[0] * n 개 만들어서 썼는 지 안썼는 지 1, 0 왔다갔다 하면서 체크 쓰면 1, 안쓰면 0
-# 
-
-
-def DFS(L, idx):
+def DFS(L, s):
   global cnt
-  if L == n:
-    cnt+=1
+  if L == m:
+    for j in range(L):
+      print(res[j], end= ' ')
+    cnt += 1
     print()
   else:
-    ch[L] = 1
-    DFS(L+1)
-    ch[L] = 0
-
-  
-
-
+    for i in range(s, n+1):
+      res[L] = i
+      DFS(L+1, i+1)
+      
 
 if __name__ =="__main__":
   n, m = map(int, input().split())
-  ch = [0]*n
+  res = [0] * (n+1)
   cnt = 0
-  DFS(0, 0)
+  DFS(0, 1)
+  print(cnt)
