26b59dc440aceede790875890996bc922c0961f4
2inlee <kbs@kakao.com>
2024-02-22 19:42:24 +0900

    돌다리건너기

---
+++ 파이썬알고리즘/DP/돌다리건너기.py
@@ -0,0 +1,15 @@
+def DFS(len):
+  if dy[len]>0: # 이미 0이 아닌 다른 값이 memorization 되어있다면 바로 출력
+    return dy[len]
+  
+  if len == 1 or len == 2:
+    return len
+  else:
+    dy[len]=DFS(len-1)+DFS(len-2)
+    return dy[len]
+
+if __name__ =="__main__":
+  n = int(input())
+  n = n+1
+  dy = [0] * (n+1)
+  print(DFS(n))
