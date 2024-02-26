b77cdb1562c5687fa9fedc42f658cbac714f487c
2inlee <kbs@kakao.com>
2024-02-26 18:40:16 +0900

    안전영역_DFS

--- 파이썬알고리즘/DFS, BFS/안전영역_BFS.py
+++ 파이썬알고리즘/DFS, BFS/안전영역_BFS.py
@@ -1,27 +1,28 @@
-from collections import deque
+def DFS(x,y,h):
+  ch[x][y] = 1
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 0 <= nx < n and 0 <= ny <= n and ch[nx][ny] == 0 and a[nx][ny] > h:
+      DFS(nx, ny, h)
 
-n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
-res = []
-dq = deque()
-# 상하좌우
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-cnt = 0
-water = 1
-for i in range(n):
-  for j in range(n):
-    if a[i][j] == 1:
-      dq.append((i, j))
-      a[i][j] = 0  # 현재 위치를 방문했다고 표시
-      while dq:
-        x, y = dq.popleft()
-        for k in range(4):
-          nx = x + dx[k]
-          ny = y + dy[k]
-          if 0 <= nx < n and 0 <= ny < n and a[nx][ny] == 1:
-            dq.append((nx, ny))
-            a[nx][ny] = 0  # 새 위치를 방문했다고 표시
-      cnt += 1
 
-print(max(res))
+if __name__ == "__main__":
+  n = int(input())
+  a = [list(map(int, input().split())) for _ in range(n)]
+  # 상하좌우
+  dx = [0, 0, -1, 1]
+  dy = [1, -1, 0, 0]
+  cnt = 0
+
+  for h in range(max(a)):
+    ch = [[0] * n for _ in range(n)]
+    for i in range(n):
+      for j in range(n):
+        if ch[i][j] == 0 and a[i][j] > h:
+          cnt += 1
+          DFS(i, j, h)
+
+    res = max(res, cnt)
+    if cnt == 0:
+      break
