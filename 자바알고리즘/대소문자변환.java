ab1ef79c3ce811a18ffb8dbbc1512be0d45b3cef
2inlee <kbs@kakao.com>
2024-05-16 20:04:14 +0900

    대소문자변환

---
+++ 자바알고리즘/대소문자변환.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+  
+public class Main {
+
+  public static String swapCase(String str) {
+    StringBuilder result = new StringBuilder();
+    
+    for (char x : str.toCharArray()) {
+      if (Character.isUpperCase(x)) {
+        result.append(Character.toLowerCase(x));
+        } else {
+          result.append(Character.toUpperCase(x));
+        }
+    }
+    
+    return result.toString();
+  }
+
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+
+    System.out.println(swapCase(str));
+    return ;
+  }
+}
