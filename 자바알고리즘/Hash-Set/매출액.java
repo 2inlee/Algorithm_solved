a00e284e8f374204676e1d1ef5dd89a9c7bb0074
2inlee <kbs@kakao.com>
2024-05-23 18:30:42 +0900

    매출액

---
+++ 자바알고리즘/Hash-Set/매출액.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+  public static ArrayList<Integer> solution(int n, int k, int[] arr) {
+    ArrayList<Integer> answer = new ArrayList<>();
+    HashMap<Integer, Integer> HM = new HashMap<>();
+    for(int i=0; i<k-1; i++){
+      HM.put(arr[i], HM.getOrDefault(arr[i], 0 ) + 1);
+    }
+    int lt=0;
+    for(int rt=k-1; rt<n; rt++){
+      HM.put(arr[rt], HM.getOrDefault(arr[rt], 0) + 1);
+      answer.add(HM.size());
+      HM.put(arr[lt], HM.get(arr[lt]) - 1);
+      if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
+      lt++;
+    }
+    
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int k = in.nextInt();
+    int[] arr = new int[n];
+
+    for(int i=0; i<n; i++){
+      arr[i] = in.nextInt();
+    }
+    for(int x : solution(n, k, arr)) System.out.print(x + " ");
+  }
+}
