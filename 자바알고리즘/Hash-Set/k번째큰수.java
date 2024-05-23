2949592f0cd95d23705fd555b4b2068ddc81b1be
2inlee <kbs@kakao.com>
2024-05-23 19:02:30 +0900

    k번째큰수 변경

--- 자바알고리즘/Hash-Set/k번째큰수.java
+++ 자바알고리즘/Hash-Set/k번째큰수.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 public class Main {
     public static int findKthLargestSum(int n, int k, int[] cards) {
-        // TreeSet을 사용하여 중복을 제거하고 자동으로 정렬
+        // TreeSet을 사용하여 중복을 제거하고 자동으로 정렬 (내림차순)
         TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
-        
         
         // 모든 가능한 3장의 카드 조합을 찾아 합을 구함
         for (int i = 0; i < n; i++) {
@@ -15,15 +14,17 @@ public class Main {
             }
         }
 
-        // set의 값을 리스트로 변환하여 접근
-        ArrayList<Integer> list = new ArrayList<>(set);
-        
-        // k번째로 큰 값을 찾음 (k는 1부터 시작하므로 index는 k-1)
-        if (k - 1 < list.size()) {
-            return list.get(k - 1);
-        } else {
-            return -1; // K번째 값이 존재하지 않으면 -1 반환
+        // TreeSet의 값을 Iterator로 접근하여 K번째 값을 찾음
+        int count = 0;
+        for (int sum : set) {
+            count++;
+            if (count == k) {
+                return sum;
+            }
         }
+        
+        // K번째 값이 존재하지 않으면 -1 반환
+        return -1;
     }
 
     public static void main(String[] args) {
