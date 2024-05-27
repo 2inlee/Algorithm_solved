1bace52eecfc41f1a201366da0bb151a1cb99a47
2inlee <kbs@kakao.com>
2024-05-27 18:07:37 +0900

    재귀함수

--- 자바알고리즘/DFS-BFS/재귀함수.java
+++ 자바알고리즘/DFS-BFS/재귀함수.java
@@ -1,18 +1,28 @@
 import java.util.*;
 
 class Main {
-  public void DFS(int n){
-    if(n == 0){
-      return ;
+  static int n;
+  static int[] ch;
+
+  public void DFS(int L){
+    if(L==n+1){
+      String tmp = "";
+      for(int i=1; i<=n; i++){
+        if(ch[i] == 1) tmp +=(i+" ");
+      }
+      if(tmp.length()>0) System.out.println(tmp);
+    }else{
+      ch[L] = 1;
+      DFS(L+1);
+      ch[L] = 0;
+      DFS(L+1);
+      }
     }
-    else{
-      System.out.println(n);
-      DFS(n-1);
-    }
-  }
 
   public static void main(String[] args){
     Main T = new Main();
-    T.DFS(3);
+    n = 3;
+    ch = new int[n+1];
+    T.DFS(1);
   }
 }
