c960baadfd3af35410b8a5d18ee5c0ccdbdd79cb
2inlee <kbs@kakao.com>
2024-05-27 17:18:08 +0900

    결정알고리즘

---
+++ 자바알고리즘/정렬/결정알고리즘.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+
+    public static int solution(int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.print(solution(m, arr));
+    }
+}
