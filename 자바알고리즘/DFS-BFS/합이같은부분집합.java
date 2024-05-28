3a6723fffe5a1390c02fc03c4ca4de2bddd758bd
2inlee <kbs@kakao.com>
2024-05-28 17:26:28 +0900

    합이같은부분수열 - cut edge

--- 자바알고리즘/DFS-BFS/합이같은부분집합.java
+++ 자바알고리즘/DFS-BFS/합이같은부분집합.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Main {
   static String answer = "NO";
-  static int n, total=0;
+  static int n, total = 0;
   boolean flag = false;
 
   public void DFS(int L, int sum, int[] arr){
     if(flag) return;
-    if(L==n){
-      if((total-sum)==sum){
+    if(sum > total / 2) return;
+    if(L == n){
+      if((total - sum) == sum){
         answer = "YES";
         flag = true;
       }
-
-    }else{
-      DFS(L+1, sum+=arr[L], arr);
-      DFS(L+1, sum, arr);
+    } else {
+      DFS(L + 1, sum + arr[L], arr); // 여기서 sum을 직접 더해서 새로운 값으로 넘김
+      DFS(L + 1, sum, arr); // 기존 sum 값 그대로 넘김
     }
   }
 
   public static void main(String[] args){
     Main T = new Main();
     Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
+    n = in.nextInt(); // static 변수를 초기화해야 함
     int[] arr = new int[n];
 
-    for(int i=0;i<n; i++){
+    for(int i = 0; i < n; i++){
       arr[i] = in.nextInt();
+      total += arr[i]; // total 값을 계산
     }
     T.DFS(0, 0, arr);
     System.out.println(answer);
